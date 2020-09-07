a160c4a5cab5ee3a40335373b5b1e4c3d6ff306e
crystalfn <969274162@qq.com>
2020-09-07 15:27:35 +0800

    [fangni] feat: implement ArrayTest - MyStack

--- src/main/java/com/twc/javaBasic/MyStack.java
+++ src/main/java/com/twc/javaBasic/MyStack.java
@@ -24,7 +24,8 @@ public class MyStack {
 
         // TODO: Please push the value into the storage here.
         // <--start
-
+        storage[count] = value;
+        count++;
         // --end-->
     }
 
@@ -37,7 +38,10 @@ public class MyStack {
         //  IMPORTANT: You SHOULD NOT USE COLLECTIONS (such as List<T>) OTHER THAN ARRAY.
         //
         // <--start
-
+        int[] newStorage = new int[newCapacity];
+        System.arraycopy(storage, 0, newStorage, 0, capacity);
+        capacity = newCapacity;
+        storage = newStorage;
         // --end-->
     }
 
@@ -57,7 +61,10 @@ public class MyStack {
         //  Please pop one element from the array. You should update the `count` field
         //  as well.
         // <--start
-
+        if (count >= 0) {
+            count--;
+            return storage[count];
+        }
         // --end-->
 
         throw new UnsupportedOperationException("Stack is empty.");
